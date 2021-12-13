b3869b2c7a2a25aad43e020b54b787fd92c9ca69
Naser ARAB <narab@verisis.com.tr>
2021-12-13 17:24:07 +0300

    Add project files.

---
+++ EtCoreApi/Entities/Expense.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EtCoreApi.Entities
+{
+    public record Expense
+    {
+        public int ExpenseId { get; init; }
+        public DateTimeOffset ExpenseDate { get; init; }
+        public decimal ExpenseAmount { get; init; }
+        public string ExpenseDetails { get; init; }
+    }
+}
---
+++ EtCoreApi/Repositories/InMemExpensesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using EtCoreApi.Entities;
+
+namespace EtCoreApi.Repositories
+{
+    public class InMemExpensesRepository
+    {
+        private readonly List<Expense> expenses = new()
+        {
+            new Expense { ExpenseId = 1, ExpenseDetails = "First expense", ExpenseDate = DateTimeOffset.UtcNow, ExpenseAmount = 700 },
+            new Expense { ExpenseId = 2, ExpenseDetails = "Second expense", ExpenseDate = DateTimeOffset.UtcNow, ExpenseAmount = 800 },
+            new Expense { ExpenseId = 2, ExpenseDetails = "Third expense", ExpenseDate = DateTimeOffset.UtcNow, ExpenseAmount = 900 },
+        };
+    }
+}
